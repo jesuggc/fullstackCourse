28a692d2de713b60e882616dcc7e066bb88727e6
jesuggc <jesuggcc@gmail.com>
2023-07-05 13:50:38 +0200

    1.4 DONE

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -37,25 +37,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  const content = [ part1, part2, part3]
-  
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
   return (
     <div>
       <Header course={course}/> 
-      <Content content ={content} />
-      <Total content = {content}/>
+      <Content content ={parts} />
+      <Total content = {parts}/>
     </div>
   )
 }
